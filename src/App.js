644830c0f7ac47cf210322ebf5f295a3bda7cd30
Graz <soladosis@rubedo.local>
2021-08-09 23:06:36 -0500

    update text

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Experience the Fountain rejuvenation experience
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://lq.systems"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          How do we reverse the entropy of the universe? 
         </a>
       </header>
     </div>
