dca015b4d9c8e78d1fc60349f0c64525f95f27b8
Graz <soladosis@rubedo.local>
2021-08-21 17:36:39 -0500

    opensea update

--- src/App.js
+++ src/App.js
@@ -18,10 +18,10 @@ function App() {
           How do we reverse the entropy of the universe?
         </a>
         <iframe src='https://opensea.io/assets/0x495f947276749ce646f68ac8c248420045cb7b5e/109525556422896821561320495960339239752497424726757471416685648908125737058305?embed=true'
-        width='100%'
-        height='100%'
         frameborder='0'
-        allowfullscreen></iframe>
+        scrolling="no"
+        onload="resizeIframe(this)"></iframe>
+        <script>  function resizeIframe(obj) {obj.style.height = obj.contentWindow.document.documentElement.scrollHeight + 'px';}</script>
       </header>
     </div>
   );
