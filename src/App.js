4e3ffe00b5cdcac39860c601637110ce0e099a59
Graz <soladosis@rubedo.local>
2021-08-21 18:08:35 -0500

    opensea update

--- src/App.js
+++ src/App.js
@@ -17,11 +17,12 @@ function App() {
         >
           How do we reverse the entropy of the universe?
         </a>
-        <iframe src='https://opensea.io/assets/0x495f947276749ce646f68ac8c248420045cb7b5e/109525556422896821561320495960339239752497424726757471416685648908125737058305?embed=true'
-        frameborder='0'
-        scrolling="no"
-        onload="resizeIframe(this)"></iframe>
-        <script>  function resizeIframe(obj) {obj.style.height = obj.contentWindow.document.documentElement.scrollHeight + 'px';}</script>
+
+      <nft-card
+      contractAddress="0x495f947276749ce646f68ac8c248420045cb7b5e"
+      tokenId="109525556422896821561320495960339239752497424726757471416685648908125737058305">
+      </nft-card>
+      <script src="https://unpkg.com/embeddable-nfts/dist/nft-card.min.js"></script>
       </header>
     </div>
   );
