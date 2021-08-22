86b589670b843d433a979dc1f334008b6f24508f
Graz <soladosis@rubedo.local>
2021-08-21 19:24:56 -0500

    opensea update

--- src/App.js
+++ src/App.js
@@ -5,7 +5,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <nft-card
+      contractAddress="0x495f947276749ce646f68ac8c248420045cb7b5e"
+      tokenId="109525556422896821561320495960339239752497424726757471416685648908125737058305">
+      </nft-card>
+        // <img src={logo} className="App-logo" alt="logo" />
+        <img src={f_logo} className="App-logo" alt="logo" />
+
         <p>
           Flow into the Fountain rejuvenation experience
         </p>
@@ -17,12 +23,6 @@ function App() {
         >
           How do we reverse the entropy of the universe?
         </a>
-
-
-      <nft-card
-      contractAddress="0x495f947276749ce646f68ac8c248420045cb7b5e"
-      tokenId="109525556422896821561320495960339239752497424726757471416685648908125737058305">
-      </nft-card>
 
       </header>
     </div>
