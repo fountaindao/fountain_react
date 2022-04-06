a922dc0a8fa34253ed0c04f4b6eabc0712b08077
gmuse <gmon01@gmail.com>
2022-04-06 10:34:12 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,9 +6,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <nft-card
-      contractAddress="0x495f947276749ce646f68ac8c248420045cb7b5e"
-      tokenId="109525556422896821561320495960339239752497424726757471416685648908125737058305">
+     <nft-card contractAddress="0x495f947276749ce646f68ac8c248420045cb7b5e" 
+      tokenId="109525556422896821561320495960339239752497424726757471416685648929016457986049"> 
       </nft-card>
         <img src={logo} className="App-logo" alt="logo" />
 
